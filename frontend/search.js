8bb216ca1f2b5c09d1ee9ec72735cfcb0664bea8
Victor Quach <varaal@gmail.com>
2017-02-18 04:49:50 +0100

    Add more related links

--- frontend/search.js
+++ frontend/search.js
@@ -27,6 +27,14 @@ function displayId(url) {
     resultsDiv.html(html);
     var links = "<p>Related links:</p>";
     links += '<p><a href="https://www.facebook.com/search/' + data.id + '/photos-of" title="Photos for this person">Photos for this profile</p> ';
+    links += '<p><a href="https://www.facebook.com/search/' + data.id + '/photos-tagged" title="Photos tagged for this person">Photos Tagged with this profile</p> ';
+    links += '<p><a href="https://www.facebook.com/search/' + data.id + '/photos-commented" title="Photos commented by this person">Photos commented by this profile</p> ';
+    links += '<p><a href="https://www.facebook.com/search/' + data.id + '/photos-liked" title="Photos liked by this person">Photos liked by this profile</p> ';
+    links += '<p><a href="https://www.facebook.com/search/' + data.id + '/stories-liked" title="Posts liked by this person">Posts liked by this profile</p> ';
+    links += '<p><a href="https://www.facebook.com/search/' + data.id + '/pages-liked" title="Pages liked by this person">Pages liked by this profile</p> ';
+    links += '<p><a href="https://www.facebook.com/search/' + data.id + '/groups" title="Groups this person belongs to">Groups this profile belongs to</p> ';
+    links += '<p><a href="https://www.facebook.com/search/' + data.id + '/events" title="Events person is going to">Events this profile is going to</p> ';
+    links += '<p><a href="https://www.facebook.com/search/' + data.id + '/places-visited" title="Places this person has visited">Places this profile has visited</p> ';
     linksDiv.html(links);
     linksDiv.show();
   }).fail(function( jqxhr, textStatus, error ) {
